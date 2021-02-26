cc697bac3816c73641805c52adc860e353e6a298
AlbinoGiraffe <lcoderl@hotmail.com>
2021-02-26 00:31:38 -0800

    Removed hashlib

--- albinobot.py
+++ albinobot.py
@@ -3,7 +3,6 @@ import datetime
 import discord
 import sys
 import os
-import hashlib
 import random
 from dotenv import load_dotenv
 
@@ -17,15 +16,15 @@ load_dotenv()
 discord_token = os.getenv("DISCORD_TOKEN")
 # print(discord_token)
 
+
 async def zodiac(question, message):
-    zhash = hashlib.md5(question.encode('utf-8'))
-    random.seed(zhash)
-    x = random.randint(0,2)
+    random.seed()
+    x = random.randint(0, 2)
     if(x == 0):
         await message.channel.send("Yes!")
         print('yes')
     else:
-        if(x==1):
+        if(x == 1):
             await message.channel.send("No!")
             print('no')
         else:
@@ -33,6 +32,7 @@ async def zodiac(question, message):
             print('idk')
 
     print('zodiac')
+
 
 async def delete_message(message):
     try:
@@ -60,15 +60,15 @@ async def on_message(message):
     if message.author == client.user:
         return
     if 'logs' not in message.channel.name:
-        print('New Message: {}'.format(message.author.id))
+        print('New Message: {}'.format(message.author.name))
 
-    # if  message.author.id == 476593458392596490:
+    # if message.author.id == 476593458392596490:
     #     await message.channel.send('SHUT THE FUCK UP <@476593458392596490>')
 
     # bot is mentioned
     if client.user.mentioned_in(message):
         print('Mentioned: \'{0}\' {1}'.format(message.content, message.author))
-        
+
         # get bot's datetime
         if "time" in message.content:
             await message.channel.send('It\'s {0}'.format(datetime.datetime.today().isoformat(' ', 'seconds')))
@@ -117,9 +117,6 @@ async def on_message(message):
             print('message: {0}'.format(op))
             # await message.channel.send("it brokey!")
             await zodiac(op, message)
-
-        
-    
 
 
 client.run(discord_token)
