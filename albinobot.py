f46f1a37656630f8c7864f434a6b4c0d49020f76
Alonso Diaz <lcoderl@hotmail.com>
2021-06-18 12:16:04 -0700

    added more fun

--- albinobot.py
+++ albinobot.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from typing import DefaultDict
 from discord.embeds import EmptyEmbed
 import uwuify
@@ -27,40 +28,51 @@ admin_id = os.getenv("ADMIN_ID")
 bot = discord.Client()
 bot = commands.Bot(command_prefix=c_prefix)
 
+
 # check that user is bot owner
 async def check_user(ctx):
-    return (ctx.message.author.id == admin_id)
+    return (ctx.message.author.id == 217644900475338752)
+
 
 # delete a message
 async def delete_message(message):
     try:
         # if(not message.author == bot.user):
-            await message.delete()
+        await message.delete()
     except:
         print("Cannot Delete '{}' NO PERMS".format(message.content))
         # await message.channel.send("Missing Permissions!")
 
+
 # generate embed for star board
 async def board_embed(message, reaction):
-    embed = discord.Embed(title=message.content, description="", color=0xe74c3c)
-    embed.add_field(name="Jump to Message", value="[Click]({})".format(message.jump_url))
+    embed = discord.Embed(title=message.content,
+                          description="",
+                          color=0xe74c3c)
+    embed.add_field(name="Jump to Message",
+                    value="[Click]({})".format(message.jump_url))
     embed.set_thumbnail(url=message.author.avatar_url)
-    timestamp = "{}/{}/{}".format(message.created_at.month, message.created_at.day, message.created_at.year)
-    embed.set_footer(text="stars: {} • {} • #{}".format(reaction.count, timestamp, message.channel.name))
+    timestamp = "{}/{}/{}".format(message.created_at.month,
+                                  message.created_at.day,
+                                  message.created_at.year)
+    embed.set_footer(text="stars: {} • {} • #{}".format(
+        reaction.count, timestamp, message.channel.name))
 
     return embed
 
+
 # set command prefix eg. ".gb"
-@ bot.command(name="cp")
-@ commands.check(check_user)
+@bot.command(name="cp")
+@commands.check(check_user)
 async def set_command_pref(ctx, n: str):
     bot.command_prefix = n
     dotenv.set_key(dotenv_file, "PREFIX", n)
     await ctx.send("New prefix is: {}".format(n))
 
+
 # reply to a message given it's id (must be in same channel)
-@ bot.command()
-@ commands.check(check_user)
+@bot.command()
+@commands.check(check_user)
 async def reply(ctx, id: int, *args):
     await ctx.trigger_typing()
     msg = ' '.join(args)
@@ -70,19 +82,21 @@ async def reply(ctx, id: int, *args):
     await m.reply(msg)
     await delete_message(ctx.message)
 
+
 # clear bot messages
-@ bot.command()
-@ commands.check(check_user)
+@bot.command()
+@commands.check(check_user)
 async def clear(ctx):
     await delete_message(ctx.message)
     async for m in ctx.message.channel.history(limit=200):
-        if(m.author == bot.user):
+        if (m.author == bot.user):
             await m.delete()
             print('Deleted \"{0}\"'.format(m.content))
 
+
 # delete x number of messages
-@ bot.command()
-@ commands.check(check_user)
+@bot.command()
+@commands.check(check_user)
 async def delete(ctx, n: int):
     # numDeleted = 0
     await delete_message(ctx.message)
@@ -95,15 +109,16 @@ async def delete(ctx, n: int):
         # except:
         #     print("Error while deleting message!")
         delete_list.append(m)
-    
+
     await ctx.channel.delete_messages(delete_list)
 
     # print("done deleting ({} messages)".format(numDeleted))
     print("done deleting ({} messages)".format(len(delete_list)))
-    
+
+
 # make bot say something
-@ bot.command()
-@ commands.check(check_user)
+@bot.command()
+@commands.check(check_user)
 async def say(ctx, *args):
     await ctx.trigger_typing()
     new_message = ' '.join(args)
@@ -112,48 +127,10 @@ async def say(ctx, *args):
     await delete_message(ctx.message)
     await ctx.send(new_message)
 
-# RNG bot
-@ bot.command(name='gb')
-async def zodiac(ctx, *args):
-    await ctx.trigger_typing()
-    random.seed()
-    x = random.randint(0, 2)
-    if(x == 0):
-        await ctx.message.channel.send("Yes!")
-    else:
-        if(x == 1):
-            await ctx.message.channel.send("No!")
-        else:
-            await ctx.message.channel.send("IDK BRUH!")
-
-# ping tool
-@ bot.command()
-async def ping(ctx):
-    await ctx.trigger_typing()
-    await ctx.send("Pong!")
-
-# get bot's datetime
-@ bot.command(name="time")
-async def get_time(ctx):
-    await ctx.trigger_typing()
-    await ctx.send('It\'s {0} PST'.format(datetime.datetime.today().isoformat(' ', 'seconds')))
-
-# github link
-@ bot.command()
-async def github(ctx):
-    await ctx.trigger_typing()
-    await ctx.send("https://github.com/AlbinoGiraffe/AlbinoBot")
-
-# the funny
-@ bot.command()
-async def pdf(ctx):
-    await ctx.trigger_typing()
-    dad = await bot.fetch_user(654564428150472714)
-    await ctx.send("{} pdf file 😳".format(dad.mention))
-    await delete_message(ctx.message)
 
 # uwuifier
-@ bot.command()
+@bot.command()
+@commands.check(check_user)
 async def uwu(ctx, *args):
     await ctx.trigger_typing()
     new_message = ' '.join(args)
@@ -163,56 +140,125 @@ async def uwu(ctx, *args):
     await delete_message(ctx.message)
     await ctx.send(uwuify.uwu(new_message))
 
-@ bot.event
+
+# RNG bot
+@bot.command(name='gb')
+async def zodiac(ctx, *args):
+    await ctx.trigger_typing()
+    random.seed()
+    x = random.randint(0, 2)
+    if (x == 0):
+        await ctx.message.channel.send("Yes!")
+    else:
+        if (x == 1):
+            await ctx.message.channel.send("No!")
+        else:
+            await ctx.message.channel.send("IDK BRUH!")
+
+
+# ping tool
+@bot.command()
+async def ping(ctx):
+    await ctx.trigger_typing()
+    await ctx.send("Pong!")
+
+
+# get bot's datetime
+@bot.command(name="time")
+async def get_time(ctx):
+    await ctx.trigger_typing()
+    await ctx.send('It\'s {0} PST'.format(datetime.datetime.today().isoformat(
+        ' ', 'seconds')))
+
+
+# github link
+@bot.command()
+async def github(ctx):
+    await ctx.trigger_typing()
+    await ctx.send("https://github.com/AlbinoGiraffe/AlbinoBot")
+
+
+# the funny
+@bot.command()
+async def pdf(ctx):
+    await ctx.trigger_typing()
+    dad = await bot.fetch_user(654564428150472714)
+    await ctx.send("{} pdf file 😳".format(dad.mention))
+    await delete_message(ctx.message)
+
+
+@bot.event
 async def on_connect():
     print('Bot connected')
 
-@ bot.event
+
+@bot.event
 async def on_ready():
     end = time.time()
     print('Logged in as {0.user}'.format(bot))
-    print("Ready in", (end-start))
+    print("Ready in", (end - start))
     print('Waiting')
 
-@ bot.event
+
+@bot.event
 async def on_message(message):
-    # check for recursion 
+    # check for recursion
     if message.author == bot.user:
         return
 
     # log messages
     if hasattr(message, 'channel'):
-        if 'logs' not in message.channel.name:
+        if isinstance(message.channel, discord.channel.DMChannel):
             print('New Message: {0}, Channel: {1}, User: {2}'.format(
-                message.content, message.channel.name, message.author.name))
+                message.content, "DM", message.author.name))
+        else:
+            if 'logs' not in message.channel.name:
+                print('New Message: {0}, Channel: {1}, User: {2}'.format(
+                    message.content, message.channel.name, message.author.name))
     else:
         print('New Message: {0}, Channel: {1}, User: {2}'.format(
             message.content, message.channel, message.author.name))
 
     # bot is mentioned
     if bot.user.mentioned_in(message):
-        print('Mentioned: \'{0}\' {1}'.format(
-            message.content, message.author.name))
-        await message.channel.send("Hi {}, i'm useless!".format(message.author.mention))
+        print('Mentioned: \'{0}\' {1}'.format(message.content,
+                                              message.author.name))
+        await message.channel.send("Hi {}, i'm useless!".format(
+            message.author.mention))
 
     # bot is DM'd
-    if isinstance(message.channel, discord.channel.DMChannel):
-        await message.channel.send("hello, i dont do anything anymore")
-    
+    # if isinstance(message.channel, discord.channel.DMChannel):
+    #     await message.channel.send("hello, i dont do anything anymore")
+
+    # emphasizes previous message
+    if (message.content.lower() == "what"):
+        m = await message.channel.history(limit=2).flatten()
+        new_msg = m[1].content
+
+        if (new_msg == new_msg.upper()):
+            new_msg = "*{}*".format(new_msg)
+        else:
+            new_msg = new_msg.upper()
+
+        await message.channel.send(new_msg)
+
     # process bot commands
     await bot.process_commands(message)
-    
-@ bot.event
-async def on_command_error(ctx, err):
-    print("Error! ({})".format(err)) 
 
-@ bot.event
+
+@bot.event
+async def on_command_error(ctx, err):
+    print("Error! ({})".format(err))
+
+
+@bot.event
 async def on_raw_reaction_add(payload):
     # pin messages
     if payload.emoji.name == "📌":
         channel = bot.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
-        reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
+        reaction = discord.utils.get(message.reactions,
+                                     emoji=payload.emoji.name)
         if reaction and reaction.count >= 2:
             await message.pin()
 
@@ -220,19 +266,21 @@ async def on_raw_reaction_add(payload):
     if payload.emoji.name == "⭐":
         channel = bot.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
-        reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
+        reaction = discord.utils.get(message.reactions,
+                                     emoji=payload.emoji.name)
         board = discord.utils.get(channel.guild.channels, name="star-board")
-        
+
         print(channel.type)
-        if(isinstance(channel, discord.abc.PrivateChannel)):
+        if (isinstance(channel, discord.abc.PrivateChannel)):
             return
-        if(channel.id == board.id):
+        if (channel.id == board.id):
             return
         if reaction and reaction.count > 1:
-            board_id = await sb.check_board(message.id) # check if message is already on board
-            embed = await board_embed(message, reaction) # generate embed
+            board_id = await sb.check_board(
+                message.id)  # check if message is already on board
+            embed = await board_embed(message, reaction)  # generate embed
 
-            if(board_id):
+            if (board_id):
                 try:
                     board_message = await board.fetch_message(board_id)
                 except:
@@ -247,10 +295,8 @@ async def on_raw_reaction_add(payload):
                 board_message = await board.send(embed=embed)
                 await sb.add_row(message.id, board_message.id)
 
-    
 
-
-# message deleted         
+# message deleted
 # @client.event
 # async def on_message_delete(message):
 #     if(message.guild.id == 760375168815071264):
@@ -273,9 +319,8 @@ async def on_raw_reaction_add(payload):
 #             embed = discord.Embed(title="Takyon edited a message", description="", color=0xe74c3c)
 #             # embed.add_field(name)
 #             embed.add_field(name="Before:", value=before.content, inline=True)
-#             embed.add_field(name="After:", value=after.content, inline=True)         
+#             embed.add_field(name="After:", value=after.content, inline=True)
 #             embed.set_footer(text="{} | #{}".format(after.created_at, after.channel.name))
 #             await channel.send(embed=embed)
-
 
 bot.run(discord_token)
